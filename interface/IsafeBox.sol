09a35dd519720ba07c6c3843a52eaea9bff89754
Pol <109659466+PVProu@users.noreply.github.com>
2025-07-19 23:16:46 +0200

    Add files via upload

--- interface/IsafeBox.sol
+++ interface/IsafeBox.sol
@@ -1,1 +1,14 @@
-njnjnjnj
+//Licencia
+// SPDX-License-Identifier: LGPL-3.0-only
+
+//Version
+pragma solidity 0.8.24;
+
+interface IsafeBox {
+    function getBalance() external view returns (uint256);
+    function withdrawEth(uint256 amount) external;
+    function depositEth() external payable;
+    function getOwner() external view returns (address);
+    function addManager(address newManager) external;
+
+}
